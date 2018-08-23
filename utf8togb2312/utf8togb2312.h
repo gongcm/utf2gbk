75f882a609e0d3cca6622d6a5c176a052a6eb369
gongcm <gongcm@linux.(none)>
2018-08-23 17:52:57 +0800

    version 1.0

---
+++ utf8togb2312/utf8togb2312.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int  UTF_8ToGB2312(char*pOut, char *pInput, int pLen);
+int UTF_8ToUnicode(char* pOutput, char *pInput);
+void UnicodeToGB2312(char*pOut, char *pInput);
